03609e489b640f623b76f053eda82c8a1a804f45
unknown <atorralb@outlook.com>
2023-10-04 15:56:40 -0600

    update

---
+++ PHP/inc/config.php
@@ -0,0 +1,8 @@
+<?php
+    $config = array(
+        "customer_suppoort_phone" => "+5213338588725",
+        "customer_service_email" => "support@axara.com",
+        "store_name" => "AXARA",
+        "store_motto" => "eyewear and lifestyle",
+    );
+?>
